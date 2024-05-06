6fa5bd76948012ee62a31f5d7eccfe2a512f9acd
Nathan <simpletimes111@gmail.com>
2024-05-05 19:22:02 -0700

    Container Communication
    The backend can access the mongodb and the frontend can access the backend. However, there are some errors with the front end I need to fix.

--- server/app.py
+++ server/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://mongo:27017/mydatabase"
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def hello_world():
+    return 'Hello, World from MongoDB!'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
